5bb8df769e1f931419f2428d365be08b57ad2034
Shradheya Thakre <tshradheya@gmail.com>
2017-12-10 09:04:59 +0530

    new syntax learnt

--- first.py
+++ first.py
@@ -1,4 +1,15 @@
 print("Hello, World!")
 x = 1;
-if x == 1:
+if x == 1 and isinstance(x,int):
     print("x=1! Indentation for braces!")
+
+#integer
+
+myString = 'hello'
+print(myString);
+a, b = 3, 4;
+
+
+myStringNone = None #wow
+
+print("String: %s" %myStringNone)
