625e59c0e0d7f425ea60a063454d86a964e285ff
Shradheya Thakre <tshradheya@gmail.com>
2017-12-10 09:13:39 +0530

    success

--- first.py
+++ first.py
@@ -1,7 +1,7 @@
 print("Hello, World!")
-x = 1;
-if x == 1 and isinstance(x,int):
-    print("x=1! Indentation for braces!")
+z = 1;
+if z == 1 and isinstance(z,int):
+    print("z=1! Indentation for braces!")
 
 #integer
 
@@ -13,3 +13,39 @@ a, b = 3, 4;
 myStringNone = None #wow
 
 print("String: %s" %myStringNone)
+
+
+#Lists
+
+myList = []
+myList.append(1);
+myList.append(21);
+
+for p in myList:
+    print(p);
+
+
+lotsofhellos = "hello" * 10 # wow wtf
+print(lotsofhellos)
+
+print([1,2,3] * 3)
+
+
+x = object()
+y = object()
+
+# TODO: change this code
+x_list = [x] * 10
+y_list = [y] * 10
+big_list = []
+big_list = x_list + y_list;
+
+print("x_list contains %d objects" % len(x_list))
+print("y_list contains %d objects" % len(y_list))
+print("big_list contains %d objects" % len(big_list))
+
+# testing code
+if x_list.count(x) == 10 and y_list.count(y) == 10:
+    print("Almost there...")
+if big_list.count(x) == 10 and big_list.count(y) == 10:
+    print("Great!")
