902fe4a9b382d7b73a0ecfdcac51ba20ae9f39ef
Shradheya Thakre <tshradheya@gmail.com>
2018-01-25 17:29:09 +0800

    game added

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # PythonScripts
 
 Learning some Python codes in free time
+
+# Color game
+
+Developed using tkinter.( Needs Python 2.7 minimum)
+
+Rule: Need to type the color in which the word is typed.
--- oop.py
+++ oop.py
@@ -1,3 +1,5 @@
+import collections
+
 class House:
     rooms = 5
 
@@ -10,3 +12,32 @@ newOne  = House()
 
 x = newOne.numOfRooms()
 print(newOne.rooms + x)
+
+people = {}
+people['a'] = 'A';
+people['z'] = 'Z';
+people['m'] = 'M';
+#people = OrderedDict()
+
+#for k,v in people.items():
+    #print (v)
+
+print ('Regular dictionary:')
+d = {}
+d['a'] = 'A'
+d['b'] = 'B'
+d['c'] = 'Z'
+d['d'] = 'D'
+d['e'] = 'E'
+d = collections.OrderedDict()
+
+d1 = d;
+for k, v in d1.items():
+    print (k, v)
+
+def fname():
+    print( '\n'.join([ ('['+ str(idx+1) +'.] ' + first_name) for idx, first_name in enumerate(d1.values()) ]) )
+
+fname()
+
+#print(fname)
